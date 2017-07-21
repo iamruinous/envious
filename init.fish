5a22ef7d518f07d5af5129bfdd6d7cb79236c297
Jade Meskill <jade.meskill@gmail.com>
2017-07-21 09:43:33 -0700

    check ~/.config/envious/env.d instead of envious.d

--- init.fish
+++ init.fish
@@ -1,5 +1,5 @@
 function init -a path --on-event init_envious
-  for file in ~/.envious.d/**
+  for file in ~/.config/envious/env.d/**
     set varname (basename "$file")
     set -q $varname
       or set -gx $varname (cat $file)
